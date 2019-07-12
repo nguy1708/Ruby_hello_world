2ac704ae53db59fbb62b63a45cda59db6ea1c11a
Hoang Nguyen <hoang.13101@gmail.com>
2019-07-12 09:15:50 -0500

    Change route to goodbye

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "¡Hola, Mundo!"
   end
+
+  def goodbye
+    render html: "This shows that I know how to change routes and application controllers.\n\nGoodbye.."
+  end
 end
